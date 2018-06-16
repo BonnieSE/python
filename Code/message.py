00321b909b2f0f32aa403b9bb8a5dc7ac240d9ee
Bo <bo.yuan.1990@gmail.com>
2018-06-16 23:23:40 +0200

    String function

---
+++ Code/message.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 16 22:47:55 2018
+
+@author: Alicia
+"""
+
+message = "Today is a Saturday!"
+print(message)
+
+name = "ada love"
+print(name.title())
+
+name = "Frida"
+print(name.upper())
+print(name.lower())
+
+first_name = "Liu"
+last_name = "li"
+full_name = first_name + " " + last_name
+print(full_name)
+
+first_name = "Liu"
+last_name = "li"
+full_name = first_name + last_name
+print(full_name)
+
+first_name = "Liu"
+last_name = "li"
+full_name = first_name.upper() + " " + last_name.upper()
+print(full_name)
+
+first_name = "Liu"
+last_name = "li"
+full_name = first_name + "" + last_name
+print(full_name)
+
+first_name = "Liu"
+last_name = "li"
+full_name = first_name + "  " + last_name
+print(full_name)
+
+first_name = "Liu"
+last_name = "li"
+full_name = first_name + " " + last_name
+message = full_name.title()
+print("hello, " + message + "!!")
+
+
