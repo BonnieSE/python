ba4da16c3717724d076942dd9ffb184e9118cf82
BonnieSE <30591819+BonnieSE@users.noreply.github.com>
2018-07-15 21:15:26 +0200

    Importing class

--- Code/class3.py
+++ Code/class3.py
@@ -5,48 +5,88 @@ Created on Sat Jul 14 16:30:23 2018
 @author: Alicia
 """
 
+
 class Car():
-  """A simple attempt to represent a car."""
-  def __init__(self, make, model, year):
-     self.make = make
-     self.model = model
-     self.year = year
-     self.odometer_reading = 0
-     
-  def get_descriptive_name(self):
-     long_name = str(self.year) + ' ' + self.make + ' ' + self.model
-     return long_name.title()
-  
-  def read_odometer(self):
-     print("This car has " + str(self.odometer_reading) + " miles on it.")
-     
-  def update_odometer(self, mileage):
-     if mileage >= self.odometer_reading:
-        self.odometer_reading = mileage
-     else:
-        print("You can't roll back an odometer!")
-        
-  def incremental_odometer(self, miles):
-      self.odometer_reading += miles
-      
+    """A simple attempt to represent a car."""
+
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        long_name = str(self.year) + ' ' + self.make + ' ' + self.model
+        return long_name.title()
+
+    def read_odometer(self):
+        print("This car has " + str(self.odometer_reading) + " miles on it.")
+
+    def update_odometer(self, mileage):
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+
+    def incremental_odometer(self, miles):
+        self.odometer_reading += miles
+
+
 class ElectricCar(Car):
-  """Represent aspects of a car, specific to electric vehicles."""
-  def __init__(self, make, model, year):
-      super().__init__(make, model, year)
-      
+    """Represent aspects of a car, specific to electric vehicles."""
+
+    def __init__(self, make, model, year):
+        super().__init__(make, model, year)
+        self.battery_size = 70
+
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print("This car has a " + str(self.battery_size) + "-kWh battery.")
+
+    def fill_gas_tank():
+        """Electric cars don't have gas tanks."""
+        print("This car doesn't need a gas tank!")
+
 my_tesla = ElectricCar('tesla', 'model s', 2016)
 print(my_tesla.get_descriptive_name())
+my_tesla.describe_battery()
+
+
+
+# Defining Attributes and Methods for the Child Class
 
 class Battery():
-  """A simple attempt to model a battery for an electric car."""
-  def __init__(self, battery_size=70):
-     """Initialize the battery's attributes."""
-     self.battery_size = battery_size
-     
-  def describe_battery(self):
-     """Print a statement describing the battery size."""
-     print("This car has a " + str(self.battery_size) + "-kWh battery.")
-     
-     
+    """A simple attempt to model a battery for an electric car."""
 
-  
+    def __init__(self, battery_size=70):
+        """Initialize the battery's attributes."""
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print("This car has a " + str(self.battery_size) + "-kWh battery.")
+
+
+class ElectricCar(Car):
+    """Represent aspects of a car, specific to electric vehicles."""
+
+    def __init__(self, make, model, year):
+        """Initialize attributes of the parent class.Then initialize attributes specific to an electric car."""
+        super().__init__(make, model, year)
+        self.battery = Battery()
+
+
+my_tesla = ElectricCar('tesla', 'model s', 2016)
+print(my_tesla.get_descriptive_name())
+my_tesla.battery.describe_battery()
+
+# Importing Classes
+from car import Car
+my_new_car = Car('audi', 'a4', 2016)
+print("\n" + my_new_car.get_descriptive_name())
+my_new_car.odometer_reading = 23
+my_new_car.read_odometer()
+my_new_car.weight(3)
+
+
+
